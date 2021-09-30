9f54e4c0a87b81cee6f03a89015f25c43f243dc2
Jhon Sebastian Bohorquez Guerrero <jhonsbg@gmail.com>
2021-09-29 20:39:29 -0500

    Se sube con git la carpeta dist/

---
+++ dist/sockets/sockets.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.obtenerUsuarios = exports.configurarUsuario = exports.mensaje = exports.desconectar = exports.conectarCliente = exports.mapaSockets = exports.mapa = exports.usuariosConectados = void 0;
+var usuarios_lista_1 = require("../classes/usuarios-lista");
+var usuario_1 = require("../classes/usuario");
+var mapa_1 = require("../classes/mapa");
+exports.usuariosConectados = new usuarios_lista_1.UsuariosLista();
+exports.mapa = new mapa_1.Mapa();
+// Eventos de mapa
+var mapaSockets = function (cliente, io) {
+    cliente.on('marcador-nuevo', function (marcador) {
+        exports.mapa.agregarMarcador(marcador);
+        cliente.broadcast.emit('marcador-nuevo', marcador);
+    });
+    cliente.on('marcador-borrar', function (id) {
+        exports.mapa.borrarMarcador(id);
+        cliente.broadcast.emit('marcador-borrar', id);
+    });
+    cliente.on('marcador-mover', function (marcador) {
+        exports.mapa.moverMarcador(marcador);
+        cliente.emit('marcador-mover', marcador);
+    });
+};
+exports.mapaSockets = mapaSockets;
+// Agregar cliente a lista de usuarios
+var conectarCliente = function (cliente) {
+    var usuario = new usuario_1.Usuario(cliente.id);
+    exports.usuariosConectados.agregar(usuario);
+};
+exports.conectarCliente = conectarCliente;
+// Escuchar si cliente se desconecta
+var desconectar = function (cliente, io) {
+    cliente.on('disconnect', function () {
+        console.log('Ciente desconectado');
+        exports.usuariosConectados.borrarUsuario(cliente.id);
+        io.emit('usuarios-activos', exports.usuariosConectados.getLista());
+    });
+};
+exports.desconectar = desconectar;
+// Escuchar mensajes
+var mensaje = function (cliente, io) {
+    cliente.on('mensaje', function (payload) {
+        console.log('Mensaje recibido', payload);
+        io.emit('mensaje-nuevo', payload);
+    });
+};
+exports.mensaje = mensaje;
+// Configurar Usuario
+var configurarUsuario = function (cliente, io) {
+    cliente.on('configurar-usuario', function (payload, callback) {
+        exports.usuariosConectados.actualizarNombre(cliente.id, payload.nombre);
+        io.emit('usuarios-activos', exports.usuariosConectados.getLista());
+        callback({
+            ok: true,
+            mensaje: "Usuario " + payload.nombre + ", configurado"
+        });
+    });
+};
+exports.configurarUsuario = configurarUsuario;
+// Obtener usuarios
+var obtenerUsuarios = function (cliente, io) {
+    cliente.on('obtener-usuarios', function () {
+        io.to(cliente.id).emit('usuarios-activos', exports.usuariosConectados.getLista());
+    });
+};
+exports.obtenerUsuarios = obtenerUsuarios;
