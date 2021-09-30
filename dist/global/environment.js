22be04afc30183ca7290b30f220a1f68389c32c4
Jhon Sebastian Bohorquez Guerrero <jhonsbg@gmail.com>
2021-09-29 21:53:08 -0500

    se elimina la carpeta dist para git

--- dist/global/environment.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Constantes o variables globales
-Object.defineProperty(exports, "__esModule", { value: true });
-//Definición del puerto - process.env.PORT lee el puerto si lo conectamos ej Heroku
-var SERVER_PORT = Number(process.env.PORT) || 5000;
-exports.default = SERVER_PORT;
